44c1499177ab15c6bf8bfa5691be423a85d00477
mcanela30 <marticanela2015@gmail.com>
2020-11-27 17:46:00 +0100

    rouing fet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PanellAdminComponent } from './panell-admin/panell-admin.component';
+import { PerfilUsuariComponent } from './perfil-usuari/perfil-usuari.component';
+import { RegistreUsuariComponent } from './registre-usuari/registre-usuari.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'inicio'},
+  {path: 'panellAdmin', component: PanellAdminComponent},
+  {path: 'perfilUsuari', component: PerfilUsuariComponent},
+  {path: 'registreUsuari', component: RegistreUsuariComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
