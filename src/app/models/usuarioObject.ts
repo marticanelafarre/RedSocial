14af17d3b86ccde3087ac4343b4eb905536e8b44
gabrigomezayuso <ggomezayuso@gmail.com>
2020-11-27 19:28:28 +0100

    Create usuarioObject.ts

---
+++ src/app/models/usuarioObject.ts
@@ -0,0 +1,12 @@
+export class usuarioObject {
+
+  constructor(
+    public nomUsuari: string,
+    public cognom: string,
+    public edat: Int16Array,
+    public foto: string,
+    public desc: string,
+    public correu: string,
+    public password: string,
+  ){}
+}
