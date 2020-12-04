465117f6c8670ae80aed56da7d2160f69a990b3f
DAW2 <ggomezayuso@gmail.com>
2020-12-04 17:37:49 +0100

    usuarios

--- src/app/perfils/perfils.component.ts
+++ src/app/perfils/perfils.component.ts
@@ -11,5 +11,22 @@ export class PerfilsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  vacio: string;
+  public contenido: string;
+  mostrarError: string = "";
+  mostrarError2: string = "";
+  mostrarError3: string = "";
+  mailPatt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  //esta funcion lo que hace es mostrarnos iferentes contenedores de el componente Eventos dependiendo de su opcion seleccionada.
+  elegirAccion(accion) {
+    //en caso de que la accion sea "anadir"
+    if (accion.target.value == "registrar") {
+      //mostrara el contenido "anadir" y asi succesivamente
+      this.contenido = "registrar"
+    } else {
+      //en caso de no ser ninguna de las 3, se mostrara un espacio vacio
+      this.vacio = "";
+    }
+  }
 }
