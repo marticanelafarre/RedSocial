586d22036b3e5d2690a45530ad9bff6a62cca947
mcanela30 <marticanela2015@gmail.com>
2020-12-04 17:35:08 +0100

    arregalt sweetalert

--- src/app/lista-admin/lista-admin.component.ts
+++ src/app/lista-admin/lista-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Character} from 'src/app/models/character.model';
+import { usuarioObject} from 'src/app/models/usuarioObject';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,16 +9,23 @@ import Swal from 'sweetalert2';
 })
 export class ListaAdminComponent implements OnInit {
 
-  ArrayAdmin: Character[]=[
+  ArrayAdmin: usuarioObject[]=[
     {
-      nombre:"marti"
-
+      nomUsuari: "Pepe",
+      cognom: "Rodriguez",
+      edat: 57,
+      foto: "",
+      desc: "Amable",
+      correu: "prova@gmail.com"
 
     },
     {
-      nombre:"alex"
-
-
+      nomUsuari: "Ete",
+      cognom: "Sech",
+      edat: 35,
+      foto: "",
+      desc: "Solidario",
+      correu: "marticanela2015@gmail.com"
     }
 ];
 
@@ -28,12 +35,24 @@ export class ListaAdminComponent implements OnInit {
   }
 
   EliminarUsuario(i){
-    this.ArrayAdmin.splice(i, 1);
-    Swal.fire(
-      'Usuario eliminado!',
-      'Usuario eliminado con éxito!',
-      'success'
-    )
-  }
 
+    Swal.fire({
+      title: '¿Seguro que lo quieres eliminar?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Si, Eliminalo!',
+    }).then((result) => {
+      this.ArrayAdmin.splice(i, 1);
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado!',
+          'Tu personaje ha sido eliminado.',
+          'success'
+        )
+      }
+    })
+
+  }
 }
--- src/app/models/usuarioObject.ts
+++ src/app/models/usuarioObject.ts
@@ -3,10 +3,10 @@ export class usuarioObject {
   constructor(
     public nomUsuari: string,
     public cognom: string,
-    public edat: Int16Array,
+    public edat: number,
     public foto: string,
     public desc: string,
     public correu: string,
-    public password: string,
+    public password?: string,
   ){}
 }
