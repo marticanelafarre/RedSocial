49ee98b5d45efba181dfe5f67c7c44477162af31
DAW2 <ggomezayuso@gmail.com>
2020-12-04 19:35:22 +0100

    Revert "Merge branch 'development-canela' into development-gomez"
    This reverts commit 2986af78ae15b668d3bd38ed9dce88ccb90ea453, reversing
    changes made to 41c501b9a92d00f746adbdc42ac1eae454bcb3e1.

--- src/app/lista-admin/lista-admin.component.ts
+++ src/app/lista-admin/lista-admin.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { usuarioObject} from 'src/app/models/usuarioObject';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lista-admin',
@@ -9,83 +7,10 @@ import Swal from 'sweetalert2';
 })
 export class ListaAdminComponent implements OnInit {
 
-  ArrayAdmin: usuarioObject[]=[
-    {
-      nomUsuari: "Pepe",
-      cognom: "Rodriguez",
-      edat: 57,
-      foto: "",
-      desc: "Amable",
-      correu: "prova@gmail.com"
-
-    },
-    {
-      nomUsuari: "Ete",
-      cognom: "Sech",
-      edat: 35,
-      foto: "",
-      desc: "Solidario",
-      correu: "marticanela2015@gmail.com"
-    },
-    {
-      nomUsuari: "Sea",
-      cognom: "Sech",
-      edat: 14,
-      foto: "",
-      desc: "Amable",
-      correu: "marticanela2015@gmail.com"
-    },
-    {
-      nomUsuari: "Lea",
-      cognom: "Sech",
-      edat: 23,
-      foto: "",
-      desc: "Solidario",
-      correu: "marticanela2015@gmail.com"
-    },
-    {
-      nomUsuari: "Pea",
-      cognom: "Sech",
-      edat: 45,
-      foto: "",
-      desc: "Amable",
-      correu: "marticanela2015@gmail.com"
-    },
-    {
-      nomUsuari: "Rea",
-      cognom: "Sech",
-      edat: 25,
-      foto: "",
-      desc: "Solidario",
-      correu: "marticanela2015@gmail.com"
-    }
-];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  EliminarUsuario(i){
-    Swal.fire({
-      title: '¿Seguro que lo quieres eliminar?',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      cancelButtonText: 'Cancelar',
-      confirmButtonText: '¡Si, Eliminalo!',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Eliminado!',
-          'Tu personaje ha sido eliminado.',
-          'success'
-        )
-        this.ArrayAdmin.splice(i, 1);
-      }
-    })
-
-
-  }
 }
